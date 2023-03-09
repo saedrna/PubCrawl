1d7780cf761e951167af369c134028ba0eff7b0d
Han <huhan8807@gmail.com>
2023-03-09 20:57:59 +0800

    add craw papers

---
+++ crawl_papers.bat
@@ -0,0 +1,47 @@
+python crawl.py -j "ISPRS Journal of Photogrammetry and Remote Sensing" -s 2018 -e 2018 -p elsevier -l 20230309-isprs-jprs-2018.csv -o 20230309-isprs-jprs-paper-2018.csv
+python crawl.py -j "ISPRS Journal of Photogrammetry and Remote Sensing" -s 2019 -e 2019 -p elsevier -l 20230309-isprs-jprs-2019.csv -o 20230309-isprs-jprs-paper-2019.csv
+python crawl.py -j "ISPRS Journal of Photogrammetry and Remote Sensing" -s 2020 -e 2020 -p elsevier -l 20230309-isprs-jprs-2020.csv -o 20230309-isprs-jprs-paper-2020.csv
+python crawl.py -j "ISPRS Journal of Photogrammetry and Remote Sensing" -s 2021 -e 2021 -p elsevier -l 20230309-isprs-jprs-2021.csv -o 20230309-isprs-jprs-paper-2021.csv
+python crawl.py -j "ISPRS Journal of Photogrammetry and Remote Sensing" -s 2022 -e 2022 -p elsevier -l 20230309-isprs-jprs-2022.csv -o 20230309-isprs-jprs-paper-2022.csv
+
+@REM python crawl_links.py -j "Remote Sensing of Environment" -s 2018 -e 2018 -o 20230309-rse-2018.csv
+@REM python crawl_links.py -j "Remote Sensing of Environment" -s 2019 -e 2019 -o 20230309-rse-2019.csv
+@REM python crawl_links.py -j "Remote Sensing of Environment" -s 2020 -e 2020 -o 20230309-rse-2020.csv
+@REM python crawl_links.py -j "Remote Sensing of Environment" -s 2021 -e 2021 -o 20230309-rse-2021.csv
+@REM python crawl_links.py -j "Remote Sensing of Environment" -s 2022 -e 2022 -o 20230309-rse-2022.csv
+
+@REM python crawl_links.py -j "IEEE Transactions on Geoscience and Remote Sensing" -s 2018 -e 2018 -o 20230309-tgrs-2018.csv
+@REM python crawl_links.py -j "IEEE Transactions on Geoscience and Remote Sensing" -s 2019 -e 2019 -o 20230309-tgrs-2019.csv
+@REM python crawl_links.py -j "IEEE Transactions on Geoscience and Remote Sensing" -s 2020 -e 2020 -o 20230309-tgrs-2020.csv
+@REM python crawl_links.py -j "IEEE Transactions on Geoscience and Remote Sensing" -s 2021 -e 2021 -o 20230309-tgrs-2021.csv
+@REM python crawl_links.py -j "IEEE Transactions on Geoscience and Remote Sensing" -s 2022 -e 2022 -o 20230309-tgrs-2022.csv
+
+@REM python crawl_links.py -j "International Journal of Geographical Information Science" -s 2018 -e 2018 -o 20230309-ijgis-2018.csv
+@REM python crawl_links.py -j "International Journal of Geographical Information Science" -s 2019 -e 2019 -o 20230309-ijgis-2019.csv
+@REM python crawl_links.py -j "International Journal of Geographical Information Science" -s 2020 -e 2020 -o 20230309-ijgis-2020.csv
+@REM python crawl_links.py -j "International Journal of Geographical Information Science" -s 2021 -e 2021 -o 20230309-ijgis-2021.csv
+@REM python crawl_links.py -j "International Journal of Geographical Information Science" -s 2022 -e 2022 -o 20230309-ijgis-2022.csv
+
+@REM python crawl_links.py -j "Journal of Geodesy" -s 2018 -e 2018 -o 20230309-jg-2018.csv
+@REM python crawl_links.py -j "Journal of Geodesy" -s 2019 -e 2019 -o 20230309-jg-2019.csv
+@REM python crawl_links.py -j "Journal of Geodesy" -s 2020 -e 2020 -o 20230309-jg-2020.csv
+@REM python crawl_links.py -j "Journal of Geodesy" -s 2021 -e 2021 -o 20230309-jg-2021.csv
+@REM python crawl_links.py -j "Journal of Geodesy" -s 2022 -e 2022 -o 20230309-jg-2022.csv
+
+@REM python crawl_links.py -j "GPS Solutions" -s 2018 -e 2018 -o 20230309-gps-2018.csv
+@REM python crawl_links.py -j "GPS Solutions" -s 2019 -e 2019 -o 20230309-gps-2019.csv
+@REM python crawl_links.py -j "GPS Solutions" -s 2020 -e 2020 -o 20230309-gps-2020.csv
+@REM python crawl_links.py -j "GPS Solutions" -s 2021 -e 2021 -o 20230309-gps-2021.csv
+@REM python crawl_links.py -j "GPS Solutions" -s 2022 -e 2022 -o 20230309-gps-2022.csv
+
+@REM python crawl_links.py -j "Annals of the American Association of Geographers" -s 2018 -e 2018 -o 20230309-aaag-2018.csv
+@REM python crawl_links.py -j "Annals of the American Association of Geographers" -s 2019 -e 2019 -o 20230309-aaag-2019.csv
+@REM python crawl_links.py -j "Annals of the American Association of Geographers" -s 2020 -e 2020 -o 20230309-aaag-2020.csv
+@REM python crawl_links.py -j "Annals of the American Association of Geographers" -s 2021 -e 2021 -o 20230309-aaag-2021.csv
+@REM python crawl_links.py -j "Annals of the American Association of Geographers" -s 2022 -e 2022 -o 20230309-aaag-2022.csv
+
+@REM python crawl_links.py -j "International Journal of Digital Earth" -s 2018 -e 2018 -o 20230309-ijde-2018.csv
+@REM python crawl_links.py -j "International Journal of Digital Earth" -s 2019 -e 2019 -o 20230309-ijde-2019.csv
+@REM python crawl_links.py -j "International Journal of Digital Earth" -s 2020 -e 2020 -o 20230309-ijde-2020.csv
+@REM python crawl_links.py -j "International Journal of Digital Earth" -s 2021 -e 2021 -o 20230309-ijde-2021.csv
+@REM python crawl_links.py -j "International Journal of Digital Earth" -s 2022 -e 2022 -o 20230309-ijde-2022.csv
